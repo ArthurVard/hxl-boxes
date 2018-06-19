5eecdec223cc8f3d1cc5dfd913cc9360a2e17949
Arthur Vardanyan <artie.vard@gmail.com>
2018-06-19 21:40:41 +0400

    optimize solution used IntMap and Set. add link to algorithms

--- lib/Boxes.hs
+++ lib/Boxes.hs
@@ -1,11 +1,15 @@
 {-
   Created       : 2018 Jun 18 (Mon) 10:06:00 PM by Arthur Vardanyan.
-  Last Modified : 2018 Jun 19 (Tue) 07:53:33 AM by Arthur Vardanyan.
+  Last Modified : 2018 Jun 19 (Tue) 09:37:40 PM by Arthur Vardanyan.
 -}
 
 module Boxes
     ( calculateJumps
     , elemAt) where
+
+import Data.List (findIndex)
+import qualified  Data.IntMap.Strict as Map
+import qualified Data.Set as Set
 
 type Index   = Int
 type JumpTo = Int
@@ -59,3 +63,93 @@ elemAt _ [] = []
 elemAt n (x:xs) | n < 0 = []
                 | n == 0 = [x]
                 | otherwise = elemAt (n-1) xs
+
+
+
+-------------------------------------------------------------------------------
+-- a bit Optimized version of my solution
+
+
+mkMap :: [Int] -> Map.IntMap Int
+mkMap = Map.fromList . zip [0..]
+
+
+calculateJumps' :: [Int] -> Maybe Int
+calculateJumps' xs  = jump 0 (mkMap xs) Set.empty
+    where
+      jump :: Int -> Map.IntMap Int -> Set.Set Int -> Maybe Int
+      jump n zs vs = case (Map.lookup n zs) of
+                       Just k  -> if Set.member k  vs
+                                  then Nothing
+                                  else jump k zs (Set.insert k vs)
+                       _       -> Just $ Set.size vs
+
+
+mlength :: Eq a => [a] -> Maybe Int
+mlength [] = Nothing
+mlenght (x:xs) =
+  let loop _ _ _ [] = Nothing
+      loop pow lam x (y:ys)
+        | x == y     = Just lam
+        | pow == lam = loop (2*pow) 1 y ys
+        | otherwise  = loop pow (1+lam) x ys
+  in loop 1 1 x xs
+
+-------------------------------------------------------------------------------
+-- Exercise
+
+infix 5 :.
+data Stream a = a :. Stream  a
+
+
+streamToList :: Stream a -> [a]
+streamToList (s:.ss) = s : streamToList ss
+
+instance Show a => Show (Stream a) where
+--    show = show . take 20 . streamToList
+    show ss = show' ss 20
+        where
+          show' :: Show a => Stream a -> Integer -> String
+          show' _ 0 = "..."
+          show' (e:.s) n = show e ++","++ show' s (n-1)
+
+
+
+-------------------------------------------------------------------------------
+-- Floyd's cycle-finding algorithm and Brent's Cycle algorithm
+
+-- https://wiki.haskell.org/Floyd's_cycle-finding_algorithm
+findCycle' :: Eq a => [a] -> ([a],[a])
+findCycle' xxs = fCycle xxs xxs
+  where fCycle (x:xs) (_:y:ys)
+         | x == y              = fStart xxs xs
+         | otherwise           = fCycle xs ys
+        fCycle _      _        = (xxs,[]) -- not cyclic
+        fStart (x:xs) (y:ys)
+         | x == y              = ([], x:fLength x xs)
+         | otherwise           = let (as,bs) = fStart xs ys in (x:as,bs)
+        fLength x (y:ys)
+         | x == y              = []
+         | otherwise           = y:fLength x ys
+
+
+-- | Brent's Cycle algorithm
+-- https://rosettacode.org/wiki/Cycle_detection#Haskell
+--
+findCycle :: Eq a => [a] -> Maybe ([a], Int, Int)
+findCycle lst =
+  do l <- findCycleLength lst
+     mu <- findIndex (uncurry (==)) $ zip lst (drop l lst)
+     let c = take l $ drop mu lst
+     return (c, l, mu)
+
+
+findCycleLength :: Eq a => [a] -> Maybe Int
+findCycleLength [] = Nothing
+findCycleLength (x:xs) =
+  let loop _ _ _ [] = Nothing
+      loop pow lam x (y:ys)
+        | x == y     = Just lam
+        | pow == lam = loop (2*pow) 1 y ys
+        | otherwise  = loop pow (1+lam) x ys
+  in loop 1 1 x xs
